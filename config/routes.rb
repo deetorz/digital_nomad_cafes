59dc3e3277181419648c6f4f791db72b49195ca7
Rhys Malyon <malyonr@gmail.com>
2021-10-26 14:19:44 +0900

    Route places index to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'places#index'
+  resources :places, only: %i[index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
