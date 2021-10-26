2ce053035943615c30d41e61f39b3647a27cd29b
Rhys Malyon <malyonr@gmail.com>
2021-10-26 13:45:48 +0900

    Update validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ApplicationRecord
   include ActionView::Helpers::TextHelper
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   validates :name, presence: true
-  validates :location, presence: true
+  validates :address, presence: true
 end
