8b2eb567d867122f54b299c468658f3fa5187c97
Rhys Malyon <malyonr@gmail.com>
2021-10-26 14:25:22 +0900

    Add 4 seed for Tokyo spaces

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,25 @@ Place.destroy_all
 
 puts "Seeding the database..."
 
-30.times do
-  Place.create(name: Faker::Restaurant.name, location: Faker::Address.full_address)
-end
+Place.create!(
+  address: '〒150-0002 東京都渋谷区渋谷２丁目２４−12',
+  name: 'WeWork Shibuya Scramble Square'
+)
+
+Place.create!(
+  address: '〒150-0041 東京都渋谷区神南１丁目６−5',
+  name: 'the Hive Jinnan'
+)
+
+Place.create!(
+  address: '〒105-0001 東京都港区虎ノ門１丁目１７−1',
+  name: 'CIC Tokyo'
+)
+
+Place.create!(
+  address: '〒153-0063 東京都目黒区目黒２丁目１１−3',
+  name: 'Impact Hub Tokyo'
+)
 
 puts "Seeded!"
 puts "#{Place.all.count} places added!"
