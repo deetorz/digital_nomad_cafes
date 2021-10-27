bb0a0b7ac325653fc67d9b7e2fe21e736cef2c34
Rhys Malyon <malyonr@gmail.com>
2021-10-27 20:41:59 +0900

    Update places_controller.rb

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -36,19 +36,3 @@ class PlacesController < ApplicationController
     end
   end
 end
-
-
-# def index
-#   @places = Place.where.not(latitude: nil, longitude: nil)
-
-#   # the `geocoded` scope filters only places with coordinates (latitude & longitude)
-#   @markers = @places.geocoded.map do |place|
-#     {
-#       lat: place.latitude,
-#       lng: place.longitude,
-#       infoWindow: { content: render_to_string(partial: "/places/info_window", locals: { place: place }) }
-#       # Uncomment the above line if you want each of your markers to display a info window when clicked
-#       # (you will also need to create the partial "/flats/map_box")
-#     }
-#   end
-# end
