cc729ef80ab325b31a32987964a6768ca45d1c9d
Rhys Malyon <malyonr@gmail.com>
2021-10-27 20:37:41 +0900

    Move map logic to places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,12 +5,50 @@ class PlacesController < ApplicationController
     # the `geocoded` scope filters only places with coordinates (latitude & longitude)
     @markers = @places.geocoded.map do |place|
       {
+        id: place.id,
         lat: place.latitude,
-        lng: place.longitude,
-        infoWindow: { content: render_to_string(partial: "/places/info_window", locals: { place: place }) }
+        lng: place.longitude
+        # infoWindow: { content: render_to_string(partial: "/places/info_window", locals: { place: place }) }
         # Uncomment the above line if you want each of your markers to display a info window when clicked
         # (you will also need to create the partial "/flats/map_box")
       }
     end
+
+    @google_api_key = ENV['GOOGLE_API_BROWSER_KEY']
+
+    if @places.count.zero?
+      @map_center = [35.6762, 139.6503] # Tokyo
+      @map_zoom = 0
+    elsif @places.count == 1
+      @map_center = [@places[0].latitude, @places[0].longitude]
+      @map_zoom = 14
+    else
+      avg_lat = 0
+      avg_lon = 0
+
+      @places.map do |place|
+        avg_lat += place.latitude
+        avg_lon += place.longitude
+      end
+
+      @map_center = [(avg_lat / @places.count), (avg_lon / @places.count)]
+      @map_zoom = 12
+    end
   end
 end
+
+
+# def index
+#   @places = Place.where.not(latitude: nil, longitude: nil)
+
+#   # the `geocoded` scope filters only places with coordinates (latitude & longitude)
+#   @markers = @places.geocoded.map do |place|
+#     {
+#       lat: place.latitude,
+#       lng: place.longitude,
+#       infoWindow: { content: render_to_string(partial: "/places/info_window", locals: { place: place }) }
+#       # Uncomment the above line if you want each of your markers to display a info window when clicked
+#       # (you will also need to create the partial "/flats/map_box")
+#     }
+#   end
+# end
