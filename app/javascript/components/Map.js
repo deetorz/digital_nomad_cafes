4428d989a6069e4926f8ec98ded490405582f7b1
Rhys Malyon <malyonr@gmail.com>
2021-10-28 18:28:09 +0900

    Migrate map and markers to new package

--- app/javascript/components/Map.js
+++ app/javascript/components/Map.js
@@ -1,27 +1,66 @@
-import React from 'react';
-import GoogleMap from 'google-map-react';
-import Marker from './Marker'
+// import React from 'react';
+// import GoogleMap from 'google-map-react';
+// import Marker from './Marker'
+
+// const Map = (props) => {
+
+//   const mapMarkers = props.markers[0].map(markerInfo => 
+//       <Marker
+//         key={markerInfo.id}
+//         lat={markerInfo.lat}
+//         lng={markerInfo.lng}
+//       />
+//     )
+
+//   return (
+//     <div style={{width: '100%', height: '400px'}}>
+//       <GoogleMap
+//         bootstrapURLKeys={{key: props.googleApiKey}}
+//         center={props.center}
+//         zoom={props.zoom}
+//       >
+//         { mapMarkers }
+//       </GoogleMap>
+//     </div>
+//   )
+// }
+
+// export default Map
+
+import React, { useState } from 'react'
+import { GoogleMap, Marker, InfoWindow } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '400px'
+};
 
 const Map = (props) => {
+  const position = {
+    lat: props.center[0],
+    lng: props.center[1],
+  };
 
   const mapMarkers = props.markers[0].map(markerInfo => 
-      <Marker
-        key={markerInfo.id}
-        lat={markerInfo.lat}
-        lng={markerInfo.lng}
-      />
-    )
-
+    <Marker
+      key={markerInfo.id}
+      animation={2}
+      position={{
+        lat: markerInfo.lat,
+        lng: markerInfo.lng,
+      }}
+    />
+  )
+  
   return (
-    <div style={{width: '100%', height: '400px'}}>
-      <GoogleMap
-        bootstrapURLKeys={{key: props.googleApiKey}}
-        center={props.center}
-        zoom={props.zoom}
-      >
-        { mapMarkers }
-      </GoogleMap>
-    </div>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={position}
+      zoom={props.zoom}
+    >
+      { /* Child components, such as markers, info windows, etc. */ }
+      { mapMarkers }
+    </GoogleMap>
   )
 }
 
