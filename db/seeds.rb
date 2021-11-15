b052b9e7586226a111c054f022b567284dafdb18
Derek Torres <derek.n.torres@gmail.com>
2021-11-15 22:14:38 +0900

    working on seeeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Place.destroy_all
+
+# puts "Seeding the database..."
+
+# Place.create!(
+#   address: '〒150-0002 東京都渋谷区渋谷２丁目２４−12',
+#   name: 'WeWork Shibuya Scramble Square'
+# )
+
+# Place.create!(
+  #   address: '〒150-0041 東京都渋谷区神南１丁目６−5',
+#   name: 'the Hive Jinnan'
+# )
+
+# Place.create!(
+  #   address: '〒105-0001 東京都港区虎ノ門１丁目１７−1',
+  #   name: 'CIC Tokyo'
+  # )
+
+  # Place.create!(
+#   address: '〒153-0063 東京都目黒区目黒２丁目１１−3',
+#   name: 'Impact Hub Tokyo'
+# )
+
+# puts "Seeded!"
+# puts "#{Place.all.count} places added!"
 require 'faker'
+require 'open-uri'
 
-
-Place.destroy_all
-
-puts "Seeding the database..."
-
-Place.create!(
-  address: '〒150-0002 東京都渋谷区渋谷２丁目２４−12',
-  name: 'WeWork Shibuya Scramble Square'
-)
-
-Place.create!(
-  address: '〒150-0041 東京都渋谷区神南１丁目６−5',
-  name: 'the Hive Jinnan'
-)
-
-Place.create!(
-  address: '〒105-0001 東京都港区虎ノ門１丁目１７−1',
-  name: 'CIC Tokyo'
-)
-
-Place.create!(
-  address: '〒153-0063 東京都目黒区目黒２丁目１１−3',
-  name: 'Impact Hub Tokyo'
-)
-
-puts "Seeded!"
-puts "#{Place.all.count} places added!"
+location_data = File.read('dataset.json')
+locations = JSON.parse(location_data)
+locations.each do |location|
+  next if location["locationName"].nil?
+  # p location["locationName"]
+  # place = Place.new
+  # place.name = location["locationName"]
+  url = "https://www.google.com/search?q=#{location['locationName'].gsub(/ /, "+")}"
+  html = URI.open(url)
+  doc = Nokogiri::HTML(html, nil, "utf-8")
+  # address =
+  p doc.text
+end
