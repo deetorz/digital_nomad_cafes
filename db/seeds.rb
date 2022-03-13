e4d15cf0c21065115371fff6700155f6bcbe0b4d
Derek Torres <derek.n.torres@gmail.com>
2022-03-14 02:19:45 +0900

    new seeds again

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Place.destroy_all
 location_data = File.read('dataset.json')
 locations = JSON.parse(location_data)
 locations.each do |location|
-  next if location["locationName"].nil? || location["address"].nil?
+  next if location["locationName"].nil? || location["address"].nil? || location["address"] == ""
   Place.create!( name: location["locationName"], address: location["address"])
 end
 
